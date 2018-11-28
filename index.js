2c93162ca09235e13477e34279c7cd8fa897129b
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-28 13:49:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,9 @@ bot.on("message", async message => {
 
 
     
-
+     const m = await message.channel.send("Ping?");
+     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
+      message.delete();
   
 
 
