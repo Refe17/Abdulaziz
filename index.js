9f5a5cfb15906a0e07edded5fd0ea8b8139f34cc
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-29 14:01:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require ("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
+const prefix = `$`
 bot.commands = new Discord.Collection();
 
 bot.on(`ready`, ()=>{
