f4ea06d650207a011e31961bb8fb2a1bfdafda27
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-29 10:51:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ bot.on("message", async message => {
   if(commandfile) commandfile.run(bot,message,args);
 
     if (message.content.startsWith(prefix + "warn")) {
+      message.delete();
       let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       let wReason = args.join(" ").slice(22);
     if(!message.member.roles.find( r => r.name === '● Discord STAFF')) return message.channel.send('This Command requires Discord STAFF Role.')
