793d34d2e345c276f91c3e45ceee9d3f4e2a72ce
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-30 18:40:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -206,30 +206,7 @@ bot.on("message", async message => {
         message.delete();
       }
     })
-    
 
-    const clean = text => {
-      if (typeof(text) === "string")
-        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-      else
-        return text;
-     }
-    
-        bot.on("message", message => {
-          const args = message.content.split(" ").slice(1);
-          if (message.content.startsWith(prefix + "eval")) {
-            if (message.author.id != "284151161291014144") return;
-            try{
-              const code = args.join(" ");
-              let evaled = eval(code);
-              if (typeof evaled !== "string")
-              evaled = require("util").inspect(evaled);
-              message.channel.send(clean(evaled), {code:"xl"});
-            }catch (err){
-              message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-          }
-        })
     bot.on('message', message=>{
       if(message.channel.name == undefined){
         if(!message.author.bot){
