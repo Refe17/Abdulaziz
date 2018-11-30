adeb9145aacba4c8bb56c08303ea14e49e247dd9
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-11-30 18:56:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,9 @@ bot.on("message", async message => {
   
       return message.channel.send(botEmbed); 
      }
-    
+    }
+
+  })
     
 
 
@@ -178,7 +180,7 @@ bot.on("message", async message => {
     return;
       })
     }
-  }
+  })
     if (message.content.startsWith(prefix + "clear")) {
       message.delete();
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You cannot do this command**")
@@ -187,7 +189,7 @@ bot.on("message", async message => {
       message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
     })
   }
-})
+
 
 
 
@@ -219,5 +221,4 @@ bot.on("message", async message => {
         }
       }
     })
-  })  
 bot.login(process.env.BOT_TOKEN)
