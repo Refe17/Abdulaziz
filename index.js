cd2e9e667963eafd7695f240e6b40116a6a8728b
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-04 18:26:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require ("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const prefix = "$"
+const prefix = "k$"
+
 bot.commands = new Discord.Collection();
 
 bot.on(`ready`, ()=>{
@@ -12,14 +13,14 @@ bot.on(`ready`, ()=>{
   console.log(`----------------`);
   console.log(`Logged in as ${bot.user.tag}!`);
   bot.user.setStatus("dnd")
-  bot.user.setGame(`$bc | Vampires`, "https://www.twitch.tv/azoqzmj")
+  bot.user.setGame(`Hi Motherfucker`, "https://www.twitch.tv/azoqzmj")
 })  
+
 
 bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
-  
-  let prefix = `$`
+  let prefix = `k$`
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
@@ -28,60 +29,34 @@ bot.on("message", async message => {
   if(commandfile) commandfile.run(bot,message,args);
 
   if(cmd === `${prefix}rape`){
-    message.delete();
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Please Mention a User")
     let rReason = args.join(" ").slice(22);
     if(!rReason) return message.channel.send("What's The Reason :thinking:")
     let rapeEmbed = new Discord.RichEmbed()
   .setColor("#96003e")
-  .setTitle("RAPE RAPE RAPE")
   .setTimestamp()
   .setImage("https://image.prntscr.com/image/SBIU91x3SZWqo_VXRJRkrw.png")
   .addField("You have succesfully raped", `${rUser}`)
-  .addField("RAPIST", `${message.author}`)
+  .addField("Rapist", `${message.author}`)
   .addField("Reason:", rReason);
   
 
 
 
  return message.channel.send(rapeEmbed); 
+  }
 
-  }
-  if (cmd === `${prefix}anime`){
-    let animeEmbed = new Discord.RichEmbed()
-      .setDescription("Anime")
-      .setTimestamp()
-      .setColor("#96003e")
-      .setImage("https://cdn.discordapp.com/attachments/516552351788826626/516664472266342411/image0.png", 200, 200);
-      return message.channel.send(animeEmbed); 
-  }
-  if (cmd === `${prefix}porn`){
-    let hubEmbed = new Discord.RichEmbed()
-      .setDescription("Porn")
-      .setTimestamp()
-      .setColor("#96003e")
-      .setImage("https://image.prntscr.com/image/tgIgtalKRYeAz_B9BGJAeg.png", 200, 200);
-    return message.channel.send(hubEmbed); 
-  }
-     if(cmd === `${prefix}help`) {
-      let helpEmbed = new Discord.RichEmbed()
-      .setDescription("Server Information - Thank you for using our bot")
-      .setTitle("Help")
-      .setTimestamp()
-      .setColor("#3498db")
-      .addField("$info","gives you info about the server") 
-    
-      return message.channel.send(helpEmbed); 
-     }
 
   if(cmd === `${prefix}ban`){
+    if (message.author.id != "502848560623255562") 
+    if (!message.author.id != "502848560623255562") return message.channel.send("Fuck off")
     message.delete();
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Please Mention a User")
     let bReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("ADMINSTRATION")) return message.channel.send("No m8 you can't do that");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No m8 you can't kick em");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("No m8 you can't kick em");
 
     let banEmbed = new Discord.RichEmbed()
       .setDescription("NEW BAN!")
@@ -93,9 +68,9 @@ bot.on("message", async message => {
       .addField("Reason:", bReason);
 
 
-    let banChannel = bot.channels.get('517612704223985666').send(banEmbed)
+    let banChannel = bot.channels.get('519225073631100948').send(banEmbed)
     if(!banChannel) return message.channel.send("Can't Find Channel");
-    message.guild.member(bUser).ban(bReason).then(message.channel.send("**DONE!**")).then(()=>{
+    message.guild.member(bUser).ban(bReason).then(message.channel.send("**We GOT EM!**")).then(()=>{
       banChannel.send(banEmbed).then(()=>{
         return;
       })
@@ -103,6 +78,8 @@ bot.on("message", async message => {
    }
   
   if (cmd === `${prefix}kick`){
+    if (message.author.id != "502848560623255562") 
+    if (!message.author.id != "502848560623255562") return message.channel.send("Fuck off")
     message.delete();
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Please Mention a User")
@@ -119,22 +96,18 @@ bot.on("message", async message => {
       .addField("Channel:", message.channel)
       .addField("Reason:", kReason);
 
-    let kickChannel = bot.channels.get('517612805608701952').send(kickEmbed)
+    let kickChannel = bot.channels.get('519225091159097346').send(kickEmbed)
     if(!kickChannel) return message.channel.send("Can't Find Channel");
-    message.guild.member(kUser).kick(kReason).then(message.channel.send("**DONE!**")).then(()=>{
+    message.guild.member(kUser).kick(kReason).then(message.channel.send("**We GOT EM!**")).then(()=>{
       kickChannel.send(kickEmbed).then(()=>{
           return;
       })
     })
- 
-
-
-
-  
-    
   }
 
     if (cmd === `${prefix}info`){
+      if (message.author.id != "502848560623255562")
+      if (!message.author.id != "502848560623255562") return message.channel.send("Fuck OFF")
       let bicon = bot.user.displayAvatarURL;
       let botEmbed = new Discord.RichEmbed()
         .setDescription("Server Information - Thank you for using our bot")
@@ -149,11 +122,11 @@ bot.on("message", async message => {
       return message.channel.send(botEmbed); 
      }
     
-    
 
 
      if (cmd === `${prefix}warn`){
-      if (message.author.id != "284151161291014144") return;
+      if (message.author.id != "502848560623255562") 
+      if (!message.author.id != "502848560623255562") return message.channel.send("Fuck off")
       let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       let wReason = args.join(" ").slice(22);
     message.delete().catch();
@@ -169,15 +142,17 @@ bot.on("message", async message => {
     .addField("Channel:", message.channel)
     .addField("Reason:", wReason);
     
-    let warnChannel = bot.channels.get('517612689565024257').send(warnEmbed)
+    let warnChannel = bot.channels.get('519225961955459073').send(warnEmbed)
     if(!warnChannel) return message.channel.send("Can't Find Channel");
       warnChannel.send(warnEmbed).then(()=>{
     return;
       })
     }
+  
   if (message.content.startsWith(prefix + "clear")) {
+    if (message.author.id != "502848560623255562") 
+    if (!message.author.id != "502848560623255562") return message.channel.send("Fuck off")
     message.delete();
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You cannot do this command**")
     if(!args[0]) return message.channel.send("Specify a Number")
     message.channel.bulkDelete(args[0]).then(()=> {
       message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
@@ -188,7 +163,9 @@ bot.on("message", async message => {
 bot.on(`message`, message=>{
   if (message.content.startsWith(prefix + "bc")) {
     if (message.author.id != "502848560623255562")
-    if (message.author.id != "284151161291014144") return;
+    if (message.author.id != "284151161291014144")
+    if (!message.author.id != "502848560623255562")
+    if (!message.author.id != "284151161291014144") return message.channel.send("Fuck OFF")
     let args = message.content.split(" ").slice(1);
     var argresult = args.join(' '); 
     message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
@@ -209,7 +186,7 @@ bot.on('message', message=>{
         .setThumbnail(message.author.avatarURL)
         .setFooter('From **' + message.author.username + "#" + message.author.discriminator + ' (' + message.author.id + ')** ')
         .setTimestamp()
-      bot.channels.get('517612714844225566').send(embedArgs)
+      bot.channels.get('519228600327602178').send(embedArgs)
     }
   }
 })
@@ -222,7 +199,8 @@ const clean = text => {
 bot.on("message", message => {
   const args = message.content.split(" ").slice(1);
   if (message.content.startsWith(prefix + "eval")) {
-    if (message.author.id != "284151161291014144") return;
+    if (message.author.id != "284151161291014144")
+    if (!message.author.id != "284151161291014144") return message.channel.send("Fuck OFF")
     try{
       const code = args.join(" ");
       let evaled = eval(code);
@@ -231,7 +209,8 @@ bot.on("message", message => {
       message.channel.send(clean(evaled), {code:"xl"});
     }catch (err){
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-}
+    }
   }
 })
+
 bot.login(process.env.BOT_TOKEN)
