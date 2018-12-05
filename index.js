56f36797683236c58526e669d57a38288116bc7e
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-05 16:56:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,11 +50,17 @@ bot.on("message", async message => {
  return message.channel.send(rapeEmbed); 
   }
  if(cmd === `${prefix}hug`){
-  message.delete();
+
   let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!hUser) return message.channel.send("Please Mention a User")
 
-let replies = ["https://cdn.discordapp.com/attachments/512896743655079946/519092315047067660/original_4.gif", "https://cdn.discordapp.com/attachments/512896743655079946/519092315801911296/jc8lJMmdskGBqyo4018jsTKcKqg.gif", "https://cdn.discordapp.com/attachments/515936545467924510/519860737296957450/hug.gif"]
+let replies = ["https://cdn.discordapp.com/attachments/517612631155146766/519874185863102480/hug_3.gif",
+ "https://sweetytextmessages.com/wp-content/uploads/2018/10/Super-Cute-Anime-Love-Gifs-1.gif", 
+ "https://cdn.discordapp.com/attachments/515936545467924510/519860737296957450/hug.gif",
+"https://cdn.discordapp.com/attachments/517612631155146766/519874189507821568/hug2.gif",
+"https://cdn.discordapp.com/attachments/517612631155146766/519874191642853386/hug_4.gif",
+"https://cdn.discordapp.com/attachments/517612631155146766/519874196558577684/hug_5.gif",
+"https://thumbs.gfycat.com/BlindOblongAmurratsnake-small.gif",]
 let result = Math.floor((Math.random() * replies.length))
 
   let hugEmbed = new Discord.RichEmbed()
