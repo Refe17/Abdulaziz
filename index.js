6d8751ac42bdffd44b52063122a5e9c971476346
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-07 03:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on("message", async message => {
     .setColor("#96003e")
     .setTimestamp()
     .setImage("https://cdn.discordapp.com/attachments/464876757472903187/520399573169209344/tenor.gif")
-    .addField(`${message.author}`,"You have succesfully Fucked", `${fUser}`)
+    .addField(`${message.author}`,"You have succesfully Fucked", `${fUser}`, true)
 
     return message.channel.send(fuckEmbed); 
   }
