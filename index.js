22acbef4563223c871380c8f29dd317813cede4e
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-10 15:12:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ bot.on("message", async message => {
     message.delete();
     if (message.guild.roles != "521646839599071242")
     if (!message.guild.roles) return message.channel.send("No")
-    let rrole = message.guild.roles.find(r => r.id === "521645534428004362");
+    let roleID = "521645534428004362";
     let member = message.mentions.members.first();
     if(!member) return message.channel.send("Please Mention a User")
-    member.addRole(rrole).catch(console.error);
+    member.addRole(roleID).catch(console.error);
     message.channel.send("**DONE**")
 
     let wgEmbed = new Discord.RichEmbed()
