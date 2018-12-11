35eb35a270ad0d898f79e4032434da81ea069a83
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-11 11:48:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -213,6 +213,22 @@ let result = Math.floor((Math.random() * replies.length))
 
 message.channel.send(flyEmbed); 
 }
+if(cmd === `${prefix}fly`){
+
+
+  let replies = ["https://cdn.discordapp.com/attachments/429271562772938755/519901058252931072/image0.gif",]
+  let result = Math.floor((Math.random() * replies.length))
+  
+    let flyEmbed = new Discord.RichEmbed()
+    .setColor("#96003e")
+    .setTimestamp()
+    .setAuthor(message.author.tag, message.author.avatarURL)
+    .setTitle("i BELIEVE I CAN FLY")
+    .setImage(replies[result])
+  
+  
+  message.channel.send(flyEmbed); 
+  }
 if(cmd === `${prefix}kill`){
 
   let killUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -456,8 +472,6 @@ bot.on("messageDelete", async message => {
   
   deletechannel.send(deleteEmbed);
 })
-
-
 
 
 
