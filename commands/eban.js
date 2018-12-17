a9e76ffc022e8eba3e1f2975d807e86d31c28dc7
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-17 12:08:44 +0300

    Update eban.js

--- commands/eban.js
+++ commands/eban.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const moment = require('moment');
 const ms = require("ms");
 const fs = require("fs")
 const bantimer = JSON.parse(fs.readFileSync("./timer/bantimer.json", "utf8"));
@@ -7,10 +7,8 @@ const banlist = JSON.parse(fs.readFileSync("./banlist.json", "utf8"));
 
 module.exports.run = async (bot, msg, args, custom_functions) => {
     msg.delete();
-
     let tagged = msg.guild.member(msg.mentions.users.first() || args[1]);
     let i = parseInt(args[2], 10)
-
     if(!tagged) return msg.reply(`الرجاء تحديد الشخص`).then(msg=>{
         msg.delete(ms(`2 seconds`));
     })
@@ -37,7 +35,7 @@ module.exports.run = async (bot, msg, args, custom_functions) => {
         .setTimestamp()
         .setThumbnail(tagged.avatarURL)
     msg.guild.member(tagged).ban(rn).then(()=>{
-        bot.channels.get('451629684199784458').send(embed).then(()=>{
+        bot.channels.get('523612546037448706').send(embed).then(()=>{
           bantimer[tagged.id] = {
             guild: msg.guild.id,
             time: Date.now() + toTime.fromSeconds(time).ms(),
@@ -46,13 +44,16 @@ module.exports.run = async (bot, msg, args, custom_functions) => {
           fs.writeFile('./timer/bantimer.json', JSON.stringify(bantimer, null, 4), err =>{
             if(err) throw err;
           })
+          let now = moment();
+          let sql = `insert into discord_bans(User, UserBy, Reason, UniqeID, Date, isBanned) values ("${tagged.id}", "${msg.author.id}", "${rn}", "${unqieID}", "${now.format('YYYY-MM-DD HH:mm:ss')}", "${true}")`;
+          con.query(sql)
           msg.reply(`Done!`)
-
         })
     })
 }
+ 
 module.exports.help = {
-  name: "eban",
+  name: "aban",
   aliases: ["ban", "b"],
   permission: 2,
 }
